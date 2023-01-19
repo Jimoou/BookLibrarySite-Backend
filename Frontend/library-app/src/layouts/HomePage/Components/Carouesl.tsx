5bf040d004e37b059d356dbfb71cb38a4c3efc0f
Jiwoon <kfromh0136@Naver.com>
2023-01-19 10:04:37 +0900

    #7-React-Routing
    리액트 라우터로 리팩토링함.

--- Frontend/library-app/src/layouts/HomePage/Components/Carouesl.tsx
+++ Frontend/library-app/src/layouts/HomePage/Components/Carouesl.tsx
@@ -2,6 +2,7 @@ import { ReturnBook } from "./ReturnBook";
 import { useEffect, useState } from "react";
 import BookModel from "../../../models/BookModel";
 import { SpinnerLoading } from "../../Utils/SpinnerLoading";
+import { Link } from "react-router-dom";
 
 export const Carousel = () => {
     const [books, setBooks] = useState<BookModel[]>([]);
@@ -111,7 +112,7 @@ export const Carousel = () => {
                 </div>
             </div>
             <div className='homepage-carousel-title mt-3'>
-                <a className='btn btn-outline-secondary btn-lg' href='#!'>View More</a>
+                <Link className='btn btn-outline-secondary btn-lg' to='/search'>View More</Link>
             </div>
         </div>
     );
