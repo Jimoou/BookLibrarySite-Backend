159588125da9a40ef53f1dd960becd07404b58cb
Jiwoon <kfromh0136@Naver.com>
2023-01-21 20:42:32 +0900

    #15-Login
    Okta를 이용해 로그인 화면과 기능을 구현했다.

--- Frontend/library-app/src/layouts/Utils/StarsReview.tsx
+++ Frontend/library-app/src/layouts/Utils/StarsReview.tsx
@@ -18,7 +18,7 @@ export const StarsReview: React.FC<{ rating: number; size: number }> = (
             } else if (rating === .5) {
                 halfStars = halfStars + 1;
                 rating = rating - .5;
-            } else if (rating == 0) {
+            } else if (rating === 0) {
                 emptyStars = emptyStars+1;
             } else {
                 break;
