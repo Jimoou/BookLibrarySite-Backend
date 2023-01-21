df184141a2dc9587872a3d9788feb18eb7b930b1
Jiwoon <kfromh0136@Naver.com>
2023-01-21 21:08:56 +0900

    #15-HomepageRefactoring
    로그인, 로그아웃 기능을 구현하고 홈페이지를 로그인 전후로 구분짓는 리팩토링 해주었다.

--- Frontend/library-app/src/layouts/HomePage/Components/LibraryServices.tsx
+++ Frontend/library-app/src/layouts/HomePage/Components/LibraryServices.tsx
@@ -1,28 +1,42 @@
-export const LibraryServices = () => {
+import { useOktaAuth } from "@okta/okta-react";
+import { Link } from "react-router-dom";
 
-    return(
-        <div className='container my-5'>
-            <div className='row p-4 align-items-center border shadow-lg'>
-                <div className='col-lg-7 p-3'>
-                    <h1 className='display-4 fw-bold'>
-                        Can't find what you are looking for?
-                    </h1>
-                    <p className='lead'>
-                        If you cannot find what you are looking for, 
-                        send our library admin's a personal message!
-                    </p>
-                    <div className='d-grid gap-2 justify-content-md-start mb-4 mb-lg-3'>
-                        <a href='/messages' type='button' className='btn btn-outline-secondary btn-lg px-4 me-md-2text-gray'>
-                            Library Services
-                        </a>   
-                        :
-                        <a type='button' className='btn btn-outline-secondary btn-lg px-4 me-md-2text-gray' href='/login'>
-                            Sign up
-                        </a> 
-                    </div>
-                </div>
-                <div className='col-lg-4 offset-lg-1 shadow-lg lost-image'></div>
-            </div>
+export const LibraryServices = () => {
+  const { authState } = useOktaAuth();
+
+  return (
+    <div className="container my-5">
+      <div className="row p-4 align-items-center border shadow-lg">
+        <div className="col-lg-7 p-3">
+          <h1 className="display-4 fw-bold">
+            Can't find what you are looking for?
+          </h1>
+          <p className="lead">
+            If you cannot find what you are looking for, send our library
+            admin's a personal message!
+          </p>
+          <div className="d-grid gap-2 justify-content-md-start mb-4 mb-lg-3">
+            {authState?.isAuthenticated ? (
+              <Link
+                to="/messages"
+                type="button"
+                className="btn btn-outline-secondary btn-lg px-4 me-md-2text-gray"
+              >
+                Library Services
+              </Link>
+            ) : (
+              <Link
+                type="button"
+                className="btn btn-outline-secondary btn-lg px-4 me-md-2text-gray"
+                to="/login"
+              >
+                Sign up
+              </Link>
+            )}
+          </div>
         </div>
-    );
-}
+        <div className="col-lg-4 offset-lg-1 shadow-lg lost-image"></div>
+      </div>
+    </div>
+  );
+};
