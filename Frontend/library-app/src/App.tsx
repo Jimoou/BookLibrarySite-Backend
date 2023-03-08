b3ac82eee2468cddaf32d3f4000e5c0eee2531c3
Jiwoon <kfromh0136@Naver.com>
2023-03-08 15:21:18 +0900

    #41-ShelfPage경로설정
    ShelfPage의 Routing 설정과 경로를 맞춰주었다.

--- Frontend/library-app/src/App.tsx
+++ Frontend/library-app/src/App.tsx
@@ -10,6 +10,7 @@ import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js";
 import { LoginCallback, Security } from "@okta/okta-react";
 import LoginWidget from "./Auth/LoginWidget";
 import { ReviewListPage } from "./layouts/BookCheckoutPage/ReviewListPage/ReviewListPage";
+import { ShelfPage } from "./layouts/ShelfPage/ShelfPage";
 
 function App() {
   const oktaAuth = new OktaAuth(oktaConfig);
@@ -47,6 +48,7 @@ function App() {
               element={<LoginWidget config={oktaConfig} />}
             />
             <Route path="/login/callback" element={<LoginCallback />} />
+            <Route path="/shelf" element={<ShelfPage />} />
           </Routes>
         </div>
         <Footer />
