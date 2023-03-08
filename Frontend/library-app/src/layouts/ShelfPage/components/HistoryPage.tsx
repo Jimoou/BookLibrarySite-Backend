004459bc6d9f120e3704ea2905227a419ba869d7
Jiwoon <kfromh0136@Naver.com>
2023-03-08 22:29:07 +0900

    #48-HistoryPage HTML/CSS

--- Frontend/library-app/src/layouts/ShelfPage/components/HistoryPage.tsx
+++ Frontend/library-app/src/layouts/ShelfPage/components/HistoryPage.tsx
@@ -1,17 +1,19 @@
 import { useOktaAuth } from "@okta/okta-react";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import HistoryModel from "../../../models/HistoryModel";
+import { Pagination } from "../../Utils/Pagination";
 import { SpinnerLoading } from "../../Utils/SpinnerLoading";
 
 export const HistoryPage = () => {
   const { authState } = useOktaAuth();
-  const [isLoadingHistory, setIsLodingHistory] = useState(true);
+  const [isLoadingHistory, setIsLoadingHistory] = useState(true);
   const [httpError, setHttpError] = useState(null);
 
-  //Histories
+  // Histories
   const [histories, setHistories] = useState<HistoryModel[]>([]);
 
-  //Pagination
+  // Pagination
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
 
@@ -36,10 +38,10 @@ export const HistoryPage = () => {
         setHistories(historyResponseJson._embedded.histories);
         setTotalPages(historyResponseJson.page.totalPages);
       }
-      setIsLodingHistory(false);
+      setIsLoadingHistory(false);
     };
     fetchUserHistory().catch((error: any) => {
-      setIsLodingHistory(false);
+      setIsLoadingHistory(false);
       setHttpError(error.message);
     });
   }, [authState, currentPage]);
@@ -57,4 +59,90 @@ export const HistoryPage = () => {
   }
 
   const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+  return (
+    <div className="mt-2">
+      {histories.length > 0 ? (
+        <>
+          <h5>Recent History:</h5>
+
+          {histories.map((history) => (
+            <div key={history.id}>
+              <div className="card mt-3 shadow p-3 mb-3 bg-body rounded">
+                <div className="row g-0">
+                  <div className="col-md-2">
+                    <div className="d-none d-lg-block">
+                      {history.img ? (
+                        <img
+                          src={history.img}
+                          width="123"
+                          height="196"
+                          alt="Book"
+                        />
+                      ) : (
+                        <img
+                          src={require("./../../../Images/BooksImages/book-luv2code-1000.png")}
+                          width="123"
+                          height="196"
+                          alt="Default"
+                        />
+                      )}
+                    </div>
+                    <div className="d-lg-none d-flex justify-content-center align-items-center">
+                      {history.img ? (
+                        <img
+                          src={history.img}
+                          width="123"
+                          height="196"
+                          alt="Book"
+                        />
+                      ) : (
+                        <img
+                          src={require("./../../../Images/BooksImages/book-luv2code-1000.png")}
+                          width="123"
+                          height="196"
+                          alt="Default"
+                        />
+                      )}
+                    </div>
+                  </div>
+                  <div className="col">
+                    <div className="card-body">
+                      <h5 className="card-title"> {history.author} </h5>
+                      <h4>{history.title}</h4>
+                      <p className="card-text">{history.description}</p>
+                      <hr />
+                      <p className="card-text">
+                        {" "}
+                        Checked out on: {history.checkoutDate}
+                      </p>
+                      <p className="card-text">
+                        {" "}
+                        Returned on: {history.returnedDate}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <hr />
+            </div>
+          ))}
+        </>
+      ) : (
+        <>
+          <h3 className="mt-3">Currently no history: </h3>
+          <Link className="btn btn-primary" to={"search"}>
+            Search for new book
+          </Link>
+        </>
+      )}
+      {totalPages > 1 && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          paginate={paginate}
+        />
+      )}
+    </div>
+  );
 };
