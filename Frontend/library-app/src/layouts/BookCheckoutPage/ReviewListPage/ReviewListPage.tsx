c76ddba70fdc37c5da45182dff67cf60b86585ec
Jiwoon <kfromh0136@Naver.com>
2023-03-08 14:25:29 +0900

    #39-리뷰페이지 로딩, 페이지네이션
    리뷰 페이지 loading, Pagination을 추가하고, get Fetch 구현.

---
+++ Frontend/library-app/src/layouts/BookCheckoutPage/ReviewListPage/ReviewListPage.tsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+import ReviewModel from "../../../models/ReviewModel";
+import { SpinnerLoading } from "../../Utils/SpinnerLoading";
+
+export const ReviewListPage = () => {
+
+  const [reviews, setReviews] = useState<ReviewModel[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
+  //Pagination
+  const [currentPage, setCurrentPage] = useState(1);
+  const [reviewsPerPage] = useState(5);
+  const [totalAmountOfReviews, setTotalAmountOfReviews] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+
+  useEffect(() => {
+    const bookId = window.location.pathname.split("/")[2];
+    const fetchBookReviews = async () => {
+      const reviewUrl: string = `http://localhost:8080/api/reviews/search/findByBookId?bookId=${bookId}&page=${currentPage - 1}&size=${reviewsPerPage}`;
+
+      const responseReviews = await fetch(reviewUrl);
+
+      if (!responseReviews.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const responseJsonReviews = await responseReviews.json();
+
+      const responseData = responseJsonReviews._embedded.reviews;
+
+      setTotalAmountOfReviews(responseJsonReviews.page.totalElements);
+      setTotalPages(responseJsonReviews.page.totalPages);
+
+      const loadedReviews: ReviewModel[] = [];
+
+      let weightedStarReviews: number = 0;
+
+      for (const key in responseData) {
+        loadedReviews.push({
+          id: responseData[key].id,
+          userEmail: responseData[key].userEmail,
+          date: responseData[key].date,
+          rating: responseData[key].rating,
+          book_id: responseData[key].bookId,
+          reviewDescription: responseData[key].reviewDescription,
+        });
+      }
+      setReviews(loadedReviews);
+      setIsLoading(false);
+    };
+    fetchBookReviews().catch((error: any) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, [currentPage]);
+
+  if (isLoading) {
+    return (
+      <SpinnerLoading />
+    )
+  }
+  if (httpError) {
+    return (
+      <div className="container m-5">
+        <p>{httpError}</p>
+      </div>
+    )
+  }
+
+  const indexOfLastReview: number = currentPage * reviewsPerPage;
+  const indexOfFirstReview: number = indexOfLastReview - reviewsPerPage;
+
+  let lastItem = reviewsPerPage * currentPage <= totalAmountOfReviews ? reviewsPerPage * currentPage: totalAmountOfReviews;
+
+  return();
+}
