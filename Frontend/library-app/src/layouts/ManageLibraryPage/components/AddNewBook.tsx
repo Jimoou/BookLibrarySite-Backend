f48567accbc2647687036fd0e60c42d0ef6de6c7
Jiwoon <kfromh0136@Naver.com>
2023-03-09 18:15:48 +0900

    #60-SubmitNewBook
    새로운책 추가하는 fetch 추가.

--- Frontend/library-app/src/layouts/ManageLibraryPage/components/AddNewBook.tsx
+++ Frontend/library-app/src/layouts/ManageLibraryPage/components/AddNewBook.tsx
@@ -1,5 +1,6 @@
 import { useOktaAuth } from "@okta/okta-react";
 import { useState } from "react";
+import AddBookRequest from "../../../models/AddBookRequest";
 export const AddNewBook = () => {
   const { authState } = useOktaAuth();
 
@@ -33,6 +34,51 @@ export const AddNewBook = () => {
     reader.onerror = function (error) {
       console.log("Error", error);
     };
+  }
+
+  async function submitNewBook() {
+    const url = `http://localhost:8080/api/admin/secure/add/book`;
+    if (
+      authState?.isAuthenticated &&
+      title !== "" &&
+      author !== "" &&
+      category !== "Category" &&
+      description !== "" &&
+      copies >= 0
+    ) {
+      const book: AddBookRequest = new AddBookRequest(
+        title,
+        author,
+        description,
+        copies,
+        category
+      );
+      book.img = selectedImage;
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(book),
+      };
+
+      const submitNewBookResponse = await fetch(url, requestOptions);
+      if (!submitNewBookResponse.ok) {
+        throw new Error("Something went wrong!");
+      }
+      setTitle("");
+      setAuthor("");
+      setDescription("");
+      setCopies(0);
+      setCategory("Category");
+      setSelectedImage(null);
+      setDisplayWarning(false);
+      setDisplaySuccess(true);
+    } else {
+      setDisplayWarning(true);
+      setDisplaySuccess(false);
+    }
   }
 
   return (
@@ -148,7 +194,11 @@ export const AddNewBook = () => {
             </div>
             <input type="file" onChange={(e) => base64ConversionForImages(e)} />
             <div>
-              <button type="button" className="btn btn-primary mt-3">
+              <button
+                type="button"
+                className="btn btn-primary mt-3"
+                onClick={submitNewBook}
+              >
                 Add Book
               </button>
             </div>
