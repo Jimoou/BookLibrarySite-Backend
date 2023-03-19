f33864c92255c80fc59803e92fc9eac6198e11c1
Jiwoon <kfromh0136@Naver.com>
2023-03-19 16:19:13 +0900

    refactor/entity-refactoring
    entity파일의 import를 single class import로 변경함.

--- Backend/spring-boot-library/src/main/java/com/reactlibraryproject/springbootlibrary/Entity/Checkout.java
+++ Backend/spring-boot-library/src/main/java/com/reactlibraryproject/springbootlibrary/Entity/Checkout.java
@@ -1,6 +1,11 @@
 package com.reactlibraryproject.springbootlibrary.Entity;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
--- Backend/spring-boot-library/src/main/java/com/reactlibraryproject/springbootlibrary/Entity/Review.java
+++ Backend/spring-boot-library/src/main/java/com/reactlibraryproject/springbootlibrary/Entity/Review.java
@@ -7,7 +7,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Date;
 
 @Entity
