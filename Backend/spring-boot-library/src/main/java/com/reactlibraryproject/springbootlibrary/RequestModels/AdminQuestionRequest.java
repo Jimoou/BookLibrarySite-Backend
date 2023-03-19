9328dd9848bed04bd6a2382275e949ed2aac8ed2
Jiwoon <kfromh0136@Naver.com>
2023-03-19 16:20:12 +0900

    refactor/requestmodel-refactoring
    리퀘스트 모델에 @AllargsConstructor 어노테이션 추가함.

--- Backend/spring-boot-library/src/main/java/com/reactlibraryproject/springbootlibrary/RequestModels/AdminQuestionRequest.java
+++ Backend/spring-boot-library/src/main/java/com/reactlibraryproject/springbootlibrary/RequestModels/AdminQuestionRequest.java
@@ -1,8 +1,10 @@
 package com.reactlibraryproject.springbootlibrary.RequestModels;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class AdminQuestionRequest {
 
     private Long id;
