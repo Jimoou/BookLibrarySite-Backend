c4644002d6e0f2982ce86964425fb9a72e5b7efb
Jiwoon <kfromh0136@Naver.com>
2023-03-22 00:36:59 +0900

    #111-success-functionally
    프론트에서 결제 승인 API 호출 fetch 코드 작성하고, 결제 내역 GET 하는 fetch 코드와 UI 구성했음.

--- Frontend/library-app/src/layouts/CartPage/components/Success.tsx
+++ Frontend/library-app/src/layouts/CartPage/components/Success.tsx
@@ -1,7 +1,6 @@
 import { useOktaAuth } from "@okta/okta-react";
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import { tossConfig } from "../../../lib/tossConfig";
 import SuccessPaymentRequest from "../../../models/SuccessPaymentRequest";
 import { SpinnerLoading } from "../../Utils/SpinnerLoading";
 
@@ -14,14 +13,18 @@ export const Success = () => {
   const paymentKey = String(searchParams.get("paymentKey"));
   const orderId = String(searchParams.get("orderId"));
   const amount = Number(searchParams.get("amount"));
+  const successPaymentRequest = new SuccessPaymentRequest(
+    paymentKey,
+    orderId,
+    amount
+  );
 
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const { approvedAt, status } = await confirmPayments();
-        await successHandler(approvedAt, status);
+        await confirmPayments();
         navigate("/success-complete");
       } catch (error: any) {
         console.log(error.message);
@@ -34,50 +37,23 @@ export const Success = () => {
   }, [authState]);
 
   const confirmPayments = async () => {
-    const url = "https://api.tosspayments.com/v1/payments/confirm";
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        Authorization: `Basic ${tossConfig.secretKey}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        paymentKey: paymentKey,
-        amount: amount,
-        orderId: orderId,
-      }),
-    };
-    const response = await fetch(url, requestOptions);
-
-    if (!response.ok) {
-      throw new Error("결제 승인 API 호출 실패");
-    }
-    const responseJson = await response.json();
-    return { approvedAt: responseJson.approvedAt, status: responseJson.status };
-  };
-
-  const successHandler = async (paymentDate: string, status: string) => {
-    const successPayment = new SuccessPaymentRequest(
-      paymentKey,
-      orderId,
-      paymentDate,
-      status,
-      amount
-    );
     if (authState && authState.isAuthenticated) {
-      const url = `${process.env.REACT_APP_API}/payment/secure/update`;
+      const url = `${process.env.REACT_APP_API}/payment/secure/confirm`;
       const requestOptions = {
-        method: "PUT",
+        method: "POST",
         headers: {
           Authorization: `Bearer ${authState.accessToken?.accessToken}`,
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(successPayment),
+        body: JSON.stringify(successPaymentRequest),
       };
-      const response = await fetch(url, requestOptions);
-      if (!response.ok) {
-        throw new Error("Something went wrong!");
-      }
+      await fetch(url, requestOptions)
+        .then((response) => {
+          console.log(response.status);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
     }
   };
 
