6545fd827bcd90f3bb7d45071ada133ab67c099e
Jiwoon <kfromh0136@Naver.com>
2023-03-22 21:37:12 +0900

    #83-coinservice-set
    코인서비스 테스트 환경을 마련하고, 스토리를 짜보았다. 이제 테스트 코드를 작성하면서 필요한 메소드나 의존성 주입관계에 유의하여, 다른 서비스 코드에 리팩토링이 이루어질 것 같다.

---
+++ src/main/java/com/reactlibraryproject/springbootlibrary/Controller/CoinController.java
@@ -0,0 +1,44 @@
+package com.reactlibraryproject.springbootlibrary.Controller;
+
+import com.reactlibraryproject.springbootlibrary.RequestModels.SuccessPaymentRequest;
+import com.reactlibraryproject.springbootlibrary.Service.CoinService;
+import com.reactlibraryproject.springbootlibrary.Utils.ExtractJWT;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("https://localhost:3000")
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/coins")
+@Tag(name = "코인", description = "코인 사용 API")
+public class CoinController {
+
+    private CoinService coinService;
+
+    @Operation(summary = "유저의 보유 코인 수")
+    @GetMapping("/secure/count")
+    public int userCoins(@RequestHeader(value = "Authorization") String token) {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        return coinService.userCoins(userEmail);
+    }
+
+    @Operation(summary = "코인 구매 성공")
+    @GetMapping("/secure/confirm")
+    public ResponseEntity<String> successPayment(
+     @RequestHeader(value = "Authorization") String token,
+     @RequestBody SuccessPaymentRequest paymentRequests)
+     {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        return coinService.successPayment(userEmail, paymentRequests);
+    }
+
+    @Operation(summary = "코인 결제")
+    @PutMapping("/secure/coin-pay")
+    public void payWithCoin(@RequestHeader(value = "Authorization") String token) {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        coinService.payWithCoin(userEmail);
+    }
+}
